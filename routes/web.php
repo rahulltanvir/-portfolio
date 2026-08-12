c2c68e209d914bf8780287ae2546ff1909aa3fa3
rahulltanvir <tanvirrahul00000@gmail.com>
2026-08-13 01:15:22 +0600

    Add Vercel runtime test route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('website.home.index');
 })->name('home');
+
+Route::get('/vercel-test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Laravel is running on Vercel',
+        'php' => PHP_VERSION,
+        'laravel' => app()->version(),
+        'environment' => app()->environment(),
+    ]);
+});
