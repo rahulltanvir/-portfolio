ac2ac84134797031906c7f886603c8f8ebdbb433
rahulltanvir <tanvirrahul00000@gmail.com>
2026-08-14 00:12:23 +0600

    Fix Laravel Vercel storage path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -18,4 +18,29 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
+    })
+    ->create();
+
+if (isset($_SERVER['VERCEL'])) {
+    $tmp = '/tmp/laravel';
+
+    if (! is_dir($tmp)) {
+        mkdir($tmp, 0777, true);
+    }
+
+    if (! is_dir($tmp.'/views')) {
+        mkdir($tmp.'/views', 0777, true);
+    }
+
+    if (! is_dir($tmp.'/cache')) {
+        mkdir($tmp.'/cache', 0777, true);
+    }
+
+    if (! is_dir($tmp.'/sessions')) {
+        mkdir($tmp.'/sessions', 0777, true);
+    }
+
+    $app->useStoragePath($tmp);
+}
+
+return $app;
