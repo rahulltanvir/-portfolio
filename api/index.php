5e93f1389e59373d209d6692466f55939b407530
rahulltanvir <tanvirrahul00000@gmail.com>
2026-08-14 14:10:53 +0600

    Fix Vercel production configuration

--- api/index.php
+++ api/index.php
@@ -1,29 +1,9 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+require __DIR__ . '/../vendor/autoload.php';
 
-try {
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-    require __DIR__ . '/../vendor/autoload.php';
-
-    $app = require_once __DIR__ . '/../bootstrap/app.php';
-
-    $app->handleRequest(
-        Illuminate\Http\Request::capture()
-    );
-
-} catch (\Throwable $e) {
-
-    http_response_code(500);
-
-    echo '<pre>';
-    echo 'ERROR: ' . get_class($e) . PHP_EOL;
-    echo 'MESSAGE: ' . $e->getMessage() . PHP_EOL;
-    echo 'FILE: ' . $e->getFile() . PHP_EOL;
-    echo 'LINE: ' . $e->getLine() . PHP_EOL;
-    echo PHP_EOL;
-    echo $e->getTraceAsString();
-    echo '</pre>';
-}
+$app->handleRequest(
+    Illuminate\Http\Request::capture()
+);
