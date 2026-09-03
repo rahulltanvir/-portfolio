8724695e56cceff0166d1696f359019df7d0457c
rahulltanvir <tanvirrahul00000@gmail.com>
2026-09-04 00:36:47 +0600

    Fix CSS asset URL

--- resources/views/website/master.blade.php
+++ resources/views/website/master.blade.php
@@ -37,11 +37,7 @@
 
     <!-- Custom CSS -->
           {{-- <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"> --}}
-          <link rel="stylesheet"
-      href="{{ app()->environment('production')
-            ? 'https://' . request()->getHost() . '/assets/css/style.css'
-            : asset('assets/css/style.css') }}">
-
+          <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     @stack('styles')
 
 </head>
